6c5222753eb945408661e919459af3e753417a73
Philani MAHELE <philanimahele@gmail.com>
2019-03-13 11:59:28 +0200

    Exciting to learn Roslyn: Try it, mind blowing for me

---
+++ RoslynClassCreationAndCompile/CreateandCompile.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Generic;
+
+namespace RoslynClassCreationAndCompile
+{
+    public class CreateandCompile
+    {
+        public string CreateandCompileRun()
+        {
+            // Creating a namespace
+            var @namespace = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.ParseName("RoslynClassCreationAndCompile")).NormalizeWhitespace();
+            @namespace = @namespace.AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.ParseName("System")));
+
+            // Public class called "DiaplayWord
+            var classDeclaration = SyntaxFactory.ClassDeclaration("DisplayWord");
+            classDeclaration = classDeclaration.AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+
+            // Just a variable
+            var variableDeclaration = SyntaxFactory.VariableDeclaration(SyntaxFactory.ParseTypeName("string"))
+                .AddVariables(SyntaxFactory.VariableDeclarator("displayMe"));
+            var fieldDeclaration = SyntaxFactory.FieldDeclaration(variableDeclaration)
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword));
+
+            // The body block of the method
+            var syntaxWrite = SyntaxFactory.ParseStatement("Console.WriteLine(message);");
+
+            // Creating a parameter for the method
+            List<ParameterSyntax> paramList = new List<ParameterSyntax>
+            {
+                SyntaxFactory.Parameter(SyntaxFactory.Identifier("message")).WithType(SyntaxFactory.ParseTypeName("string"))
+            };
+
+            // A method called "Word", with a parameter: public void Word(string message)
+            var methodDeclaration = SyntaxFactory.MethodDeclaration(SyntaxFactory.ParseTypeName("void"), "Word")
+                .AddModifiers(SyntaxFactory.Token(SyntaxKind.PublicKeyword))
+                .AddParameterListParameters(paramList.ToArray())
+                .WithBody(SyntaxFactory.Block(syntaxWrite));
+
+            classDeclaration = classDeclaration.AddMembers(fieldDeclaration, methodDeclaration);
+
+            @namespace = @namespace.AddMembers(classDeclaration);
+
+            var code = @namespace
+                .NormalizeWhitespace()
+                .ToFullString();
+
+            return code;
+        }
+    }
+}
---
+++ RoslynClassCreationAndCompile/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace RoslynClassCreationAndCompile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Here I am creating a Compilation object
+            CreateandCompile obj = new CreateandCompile();
+            SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(obj.CreateandCompileRun()); // Call the CreateandComplie Method
+
+            string assemblyName = Path.GetRandomFileName();
+            MetadataReference[] references = new MetadataReference[]
+            {
+            MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+            MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
+            };
+
+
+            CSharpCompilation compilation = CSharpCompilation.Create(
+                assemblyName,
+                syntaxTrees: new[] { syntaxTree },
+                references: references,
+                options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary
+                ));
+
+            // To run the compilation will use Emit and pass a Stream object to give me a EmitResult object
+            using (var ms = new MemoryStream())
+            {
+                EmitResult result = compilation.Emit(ms);
+
+                if (!result.Success)
+                {
+                    IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                    diagnostic.IsWarningAsError ||
+                    diagnostic.Severity == DiagnosticSeverity.Error);
+
+                    foreach (Diagnostic diagnostic in failures)
+                    {
+                        Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                    }
+                }
+                else
+                {
+                    ms.Seek(0, SeekOrigin.Begin);
+                    Assembly assembly = Assembly.Load(ms.ToArray());
+                    
+                    // If successful I load the bytes into an Assembly object and use Reflection to Invoke the class 
+                    Type type = assembly.GetType("RoslynClassCreationAndCompile.DisplayWord");
+                    object objct = Activator.CreateInstance(type);
+                    type.InvokeMember("Word",
+                        BindingFlags.Default | BindingFlags.InvokeMethod,
+                        null,
+                        objct,
+                        new object[] { "Hello World" });
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
